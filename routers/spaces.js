e41e5a4dd24634f3e59e8ed6a5977b1ee17e3068
MartaPanasyuk <mart.panasyuk@gmail.com>
2022-06-24 11:18:42 +0200

    adding code

--- routers/spaces.js
+++ routers/spaces.js
@@ -27,4 +27,28 @@ router.get("/:id", async (req, res, next) => {
   }
 });
 
+//Cretae a story
+router.post("/:id", async (req, res, next) => {
+  const { name, content, imageUrl, token } = req.body;
+  if (!name) {
+    return res.status(400).send("Your story should have a name!");
+  }
+
+  try {
+    const userId = parseInt(req.params.id);
+    const newStory = await Story.create({
+      name,
+      content,
+      imageUrl,
+    });
+
+    const fullSpace = await Space.findByPk(newStory.id, {
+      include: [{ model: Story }],
+    });
+    res.send({ story: fullSpace });
+  } catch (e) {
+    next(e);
+  }
+});
+
 module.exports = router;
